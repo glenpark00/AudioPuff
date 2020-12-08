672a1cb3e9957863a7c6bf23a22499cdbbe14148
Glen Park <glenpark00@gmail.com>
2020-12-07 18:56:37 -0800

    Fix discover layout

--- frontend/components/discover/discover.jsx
+++ frontend/components/discover/discover.jsx
@@ -6,7 +6,6 @@ import SideBarSection from '../sidebar/sidebar_section';
 import SideBarUserItem from '../sidebar/sidebar_user_item';
 import { FaUserFriends, FaHeart } from 'react-icons/fa';
 import SideBarSongItem from '../sidebar/sidebar_song_item';
-// import ReactLoading from 'react-loading';
 
 export default class Discover extends React.Component {
   componentDidMount() {
@@ -46,54 +45,56 @@ export default class Discover extends React.Component {
     return (
       <div className='discover-page-background'>
         <div className='discover-page'>
-          <div className='page-main-content'>
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>AudioPuff: Trending</h2>
-              <div className='discover-subheader'>Up-and-coming tracks on AudioPuff</div>
+          <div className='page-full-content'>
+            <div className='page-main-content'>
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>AudioPuff: Trending</h2>
+                <div className='discover-subheader'>Up-and-coming tracks on AudioPuff</div>
+              </div>
+              <Carousel songs={trending} users={users} type='songs' />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>The Upload</h2>
+                <div className='discover-subheader'>Newly posted tracks. Just for you</div>
+              </div>
+              <Playlist songs={theUpload} users={users} />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>Up Next</h2>
+                <div className='discover-subheader'>Emerging creators and artists to follow</div>
+              </div>
+              <Carousel users={allUsers} type='users' />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>Hip Hop &#38; Rap</h2>
+                <div className='discover-subheader'>The latest and hottest hip-hop &#38; rap</div>
+              </div>
+              <Carousel songs={hipHop} users={users} type='songs' />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>Electronic</h2>
+                <div className='discover-subheader'>The latest and hottest electronic music</div>
+              </div>
+              <Carousel songs={electronic} users={users} type='songs' />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>R&#38;B &#38; Soul</h2>
+                <div className='discover-subheader'>The latest and hottest R&#38;B &#38; Soul</div>
+              </div>
+              <Carousel songs={rAndB} users={users} type='songs' />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>Pop</h2>
+                <div className='discover-subheader'>The latest and hottest pop music</div>
+              </div>
+              <Carousel songs={pop} users={users} type='songs' />
+              <div className='discover-heading-container'>
+                <h2 className='discover-header'>Rock</h2>
+                <div className='discover-subheader'>The latest and hottest rock</div>
+              </div>
+              <Carousel songs={rock} users={users} type='songs' />
             </div>
-            <Carousel songs={trending} users={users} type='songs' />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>The Upload</h2>
-              <div className='discover-subheader'>Newly posted tracks. Just for you</div>
-            </div>
-            <Playlist songs={theUpload} users={users} />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>Up Next</h2>
-              <div className='discover-subheader'>Emerging creators and artists to follow</div>
-            </div>
-            <Carousel users={allUsers} type='users' />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>Hip Hop &#38; Rap</h2>
-              <div className='discover-subheader'>The latest and hottest hip-hop &#38; rap</div>
-            </div>
-            <Carousel songs={hipHop} users={users} type='songs' />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>Electronic</h2>
-              <div className='discover-subheader'>The latest and hottest electronic music</div>
-            </div>
-            <Carousel songs={electronic} users={users} type='songs' />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>R&#38;B &#38; Soul</h2>
-              <div className='discover-subheader'>The latest and hottest R&#38;B &#38; Soul</div>
-            </div>
-            <Carousel songs={rAndB} users={users} type='songs' />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>Pop</h2>
-              <div className='discover-subheader'>The latest and hottest pop music</div>
-            </div>
-            <Carousel songs={pop} users={users} type='songs' />
-            <div className='discover-heading-container'>
-              <h2 className='discover-header'>Rock</h2>
-              <div className='discover-subheader'>The latest and hottest rock</div>
-            </div>
-            <Carousel songs={rock} users={users} type='songs' />
-          </div>
-          <div className='side-bar'>
-            <SideBarMyInfo />
-            <SideBarSection icon={<FaUserFriends />} items={whoToFollow} component={<SideBarUserItem />} text='Who to follow' />
-            <SideBarSection icon={<FaHeart />} items={likedSongs} component={<SideBarSongItem />} text={`${likedSongs.length} likes`} url='/you/likes' />
-            <div className='page-border-container'>
-              <div className='page-top-border'></div>
+            <div className='side-bar'>
+              <SideBarMyInfo />
+              <SideBarSection icon={<FaUserFriends />} items={whoToFollow} component={<SideBarUserItem />} text='Who to follow' />
+              <SideBarSection icon={<FaHeart />} items={likedSongs} component={<SideBarSongItem />} text={`${likedSongs.length} likes`} url='/you/likes' />
+              <div className='page-border-container'>
+                <div className='page-top-border'></div>
+              </div>
             </div>
           </div>
         </div>
